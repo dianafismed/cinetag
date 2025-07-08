98faf13af5422b0e7532bf97c768ac58c27526a3
Diana Avila <missernightangel@gmail.com>
2025-07-08 19:08:23 -0300

    card

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -2,6 +2,7 @@ import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
+import Card from "components/Card";
 
 function Inicio() {
   return (
@@ -11,6 +12,7 @@ function Inicio() {
       <Titulo>
         <h1>Um lugar para guardar seus vídeo e filmes!!s</h1>
       </Titulo>
+      <Card id='1' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format-src&type=png' />
       <Rodape />
     </>
   );
