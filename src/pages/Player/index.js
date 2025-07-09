be5e0bb6cad986ab4dca55f17fa96f15f5bb0546
Diana Avila <missernightangel@gmail.com>
2025-07-09 20:29:36 -0300

    not found

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -1,16 +1,19 @@
 import Banner from 'components/Banner';
 import Titulo from 'components/Titulo';
-import styles from './Player.module.css';
 import { useParams } from 'react-router-dom';
 import videos from 'json/db.json';
+import styles from './Player.module.css';
+import NaoEncontrada from 'pages/NaoEncontrada';
 
 function Player() {
-
   const parametros = useParams();
   const video = videos.find((video) => {
     return video.id === Number(parametros.id);
   })
-  console.log(video);
+
+  if (!video) {
+    return <NaoEncontrada />
+  }
 
   return (
     <>
@@ -18,14 +21,15 @@ function Player() {
       <Titulo>
         <h1>Player</h1>
       </Titulo>
-      <section className={styles.player}>
+      <section className={styles.container}>
         <iframe
           width="100%"
           height="100%"
           src={video.link}
           title={video.titulo}
           frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen>
         </iframe>
       </section>
     </>
