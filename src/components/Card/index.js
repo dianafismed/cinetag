f61ddd199a36c8678eb544b86074be3b004c3e82
Diana Avila <missernightangel@gmail.com>
2025-07-09 12:08:55 -0300

    up

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,26 @@
+import { useFavoritoContext } from 'contextos/Favoritos';
 import styles from './Card.module.css';
 import iconeFavoritar from './favoritar.png';
+import iconeDesfavoritar from './desfavoritar.png';
 
 function Card({ id, titulo, capa }) {
+  // Importando o contexto de favoritos
+  const { favorito, adicionarFavorito } = useFavoritoContext();
+  const ehFavorito = favorito.some((fav) => fav.id === id);
+  const icone = !ehFavorito ? iconeFavoritar : iconeDesfavoritar;
+
   return (
     <div className={styles.container}>
       <img src={capa} alt={titulo} className={styles.capa} />
       <h2>{titulo}</h2>
-      <img src={iconeFavoritar}
+      <img src={icone}
         alt="Favoritar"
-        className={styles.favoritar} />
+        className={styles.favoritar}
+        onClick={() => {
+          adicionarFavorito({ id, titulo, capa })
+        }}
+      />
     </div>
-
   )
 }
 
